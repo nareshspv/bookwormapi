53df5592eca4e9d61da5f8257c4093a1df9a03f7
nareshspv <nareshspv@gmail.com>
2018-08-09 20:44:52 +0530

    login

--- src/index.js
+++ src/index.js
@@ -1,12 +1,21 @@
 import express from "express";
 import path from "path";
+import mongoose from "mongoose";
+import bodyParser from "body-parser";
+import dotenv from "dotenv";
+import Promise from "bluebird";
+import auth from "./routes/auth";
+
+dotenv.config();
 const app = express();
+app.use(bodyParser.json());
+mongoose.promise = Promise;
+mongoose.connect(
+  process.env.MONGODB_URL,
+  { useMongoClient: true }
+);
 
-app.post("/api/auth", (req, res) => {
-  res.status(400).json({ errors: { global: "Invalid credentials" } });
-});
+app.use("/api/auth", auth);
 
-app.get("/*", (req, res) => {
-  res.sendFile(path.join(__dirname, "index.html"));
-});
+app.get("/*", (req, res) => res.sendFile(path.join(__dirname, "index.html")));
 app.listen(8080, () => console.log("Running on localhost:8080"));
